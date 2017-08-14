47c0c5a1dadf11aa446f20797469dfdacd3da90c
Michael Hallquist <michael.hallquist@gmail.com>
2017-08-13 22:11:34 -0400

    A few tweaks to schedule and content

--- docs/R/make-site.R
+++ docs/R/make-site.R
@@ -13,6 +13,8 @@ rmarkdown::render("talks/r-eproducible-science.Rmd",
 source("R/update-survey.R")
 source("R/Make_test_survey.R")
 
+rmarkdown::render("papaja_demo/gilmore-lebreton-hallquist.Rmd")
+
 # Since James' talk is now in html_notebook, it expects
 # to be run interactively. This breaks rmarkdown::render()
 # 
